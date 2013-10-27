4aa7db3be7e7fb59364a7e88b2a2fa18b3678c72
Nikko Schaff <nikko.schaff@gmail.com>
2013-10-26 23:12:26 -0400

    modified gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShippingRates::Application.config.secret_key_base = '17dfff0dc8a7db042a3846ca7272dba4cdce546b07c20e7e557e4e21c568d2fb5c32677f73a2c93910fbd9c9c99594d684289ac4f0813ec2918ed6530f6c2cdc'
